801d50e0a6887a95f584f6168a132be743ab9719
Trung Ly <Jun.Ly@dal.ca>
2025-02-18 19:38:36 -0400

    added data to csv, bootstrap themes

---
+++ README.md
@@ -0,0 +1,2 @@
+https://www.uniqhotels.com/blog/the-biggest-hotels-in-the-world/
+https://startbootstrap.com/previews/business-casual
---
+++ templates/footer.php
@@ -0,0 +1,8 @@
+
+        <footer class="footer text-faded text-center py-5">
+            <div class="container"><p class="m-0 small">Copyright &copy; Hotel 2025</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    </body>
+</html>
